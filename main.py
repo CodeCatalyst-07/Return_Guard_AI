dd2f86df8f9632bbfb45ac55ba70c8c061f90036
Aalekh Maheshwari <aalekhmaheshwari@gmail.com>
2026-03-01 08:43:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+# from fastapi import FastAPI
+# import antigravity  # because why not 🚀
+
+# app = FastAPI()
+
+# @app.get("/")
+# def read_root():
+#     return {"message": "Return Guard AI backend is running 🚀"}
+
+# @app.get("/health")
+# def health():
+#     return {"status": "healthy"}
 from fastapi import FastAPI
-import antigravity  # because why not 🚀
+from fastapi.middleware.cors import CORSMiddleware
+import antigravity
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://return-guard-ai-j3n4.vercel.app"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
